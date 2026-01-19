8a09f737c376a977764c87011390083a087f65ba
Ellensramalho <ellen_santos_bsf@hotmail.com>
2026-01-19 16:40:20 -0300

    Criação de novo elemento

--- script.js
+++ script.js
@@ -20,5 +20,13 @@ console.log(lista.children[lista.children.length -1]);
 const contato = document.querySelector('.contato');
 const titulo = contato.querySelector('.titulo');
 const animais = document.querySelector('.animais');
+const mapa= document.querySelector('.mapa');
 
 contato.insertBefore(animais, titulo);
+
+const novoh1 = document.createElement('h1');
+console.log(novoh1);
+novoh1.innerHTML = 'Caninos são fofos';
+novoh1.classList.add('titulo');
+
+mapa.appendChild(novoh1);
