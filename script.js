85c93f374ebd4dda097d41d96f72aa6903856844
Ellensramalho <ellen_santos_bsf@hotmail.com>
2026-01-19 15:44:14 -0300

    EventListener e window.

--- script.js
+++ script.js
@@ -14,5 +14,20 @@ const animaisLista = document.querySelector('.animais-lista');
 function callback3(e){
   console.log(e.currentTarget);
   console.log(e.target);
+  console.log(e.type); 
+  console.log(this);
 }
   animaisLista.addEventListener('click', callback3);
+
+const h1 = document.querySelector('h1');
+
+function handleEvent(e){
+  console.log(e.type, e);
+}
+
+h1.addEventListener('click', handleEvent);
+h1.addEventListener('mouseenter', handleEvent);
+h1.addEventListener('mouseout', handleEvent);
+h1.addEventListener('mousemove', handleEvent);
+window.addEventListener('scroll', handleEvent);
+window.addEventListener('resize', handleEvent);
