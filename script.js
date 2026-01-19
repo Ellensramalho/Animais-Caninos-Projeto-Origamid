530b971258553e927287bb6e3e84dc817043967f
Ellensramalho <ellen_santos_bsf@hotmail.com>
2026-01-19 16:10:26 -0300

    InnerHTML e outerHTML

--- script.js
+++ script.js
@@ -1,43 +1,10 @@
-const img = document.querySelector('img');
-
-function callback(){
-  console.log('clicou');
-}
-img.addEventListener('click', callback);
-
-function callback2(e){
-  console.log(e);
-}
-img.addEventListener('click', callback2);
-
-const animaisLista = document.querySelector('.animais-lista');
-function callback3(e){
-  console.log(e.currentTarget);
-  console.log(e.target);
-  console.log(e.type); 
-  console.log(this);
-}
-  animaisLista.addEventListener('click', callback3);
-
 const h1 = document.querySelector('h1');
+const animaisLista = document.querySelector('.animais-descricao');
 
-function handleEvent(e){
-  console.log(e.type, e);
-}
+h1.innerHTML;
+console.log(h1.innerHTML);
+console.log(animaisLista.innerHTML);
 
-h1.addEventListener('click', handleEvent);
-h1.addEventListener('mouseenter', handleEvent);
-h1.addEventListener('mouseout', handleEvent);
-h1.addEventListener('mousemove', handleEvent);
-window.addEventListener('scroll', handleEvent);
-window.addEventListener('resize', handleEvent);
+h1.outerHTML = '<p>Novo texto</p>';
+console.log(h1.outerHTML);
 
-function handleKeyboard(e){
-  if(e.key === 'a'){
-   document.body.classList.toggle('azul')
-  }
-  else if(e.key === 'v'){
-    document.body.classList.toggle('rosa')
-  }
-}
-window.addEventListener('keydown', handleKeyboard);
---
+++ seletores4.js
@@ -0,0 +1,43 @@
+const img = document.querySelector('img');
+
+function callback(){
+  console.log('clicou');
+}
+img.addEventListener('click', callback);
+
+function callback2(e){
+  console.log(e);
+}
+img.addEventListener('click', callback2);
+
+const animaisLista = document.querySelector('.animais-lista');
+function callback3(e){
+  console.log(e.currentTarget);
+  console.log(e.target);
+  console.log(e.type); 
+  console.log(this);
+}
+  animaisLista.addEventListener('click', callback3);
+
+const h1 = document.querySelector('h1');
+
+function handleEvent(e){
+  console.log(e.type, e);
+}
+
+h1.addEventListener('click', handleEvent);
+h1.addEventListener('mouseenter', handleEvent);
+h1.addEventListener('mouseout', handleEvent);
+h1.addEventListener('mousemove', handleEvent);
+window.addEventListener('scroll', handleEvent);
+window.addEventListener('resize', handleEvent);
+
+function handleKeyboard(e){
+  if(e.key === 'a'){
+   document.body.classList.toggle('azul')
+  }
+  else if(e.key === 'v'){
+    document.body.classList.toggle('rosa')
+  }
+}
+window.addEventListener('keydown', handleKeyboard);
