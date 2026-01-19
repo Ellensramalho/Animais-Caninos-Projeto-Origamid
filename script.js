11e9b2405920a1dfc68516c3fad6bd1ca18bdba2
Ellensramalho <ellen_santos_bsf@hotmail.com>
2026-01-19 15:26:55 -0300

    Novos seletores

--- script.js
+++ script.js
@@ -1,20 +1,18 @@
-const menu = document.querySelector('.menu');
-console.log(menu.classList);
-
-menu.classList.add('ativo','teste');
-menu.classList.remove('azul')
-menu.classList.toggle('azul');
-
-console.log(menu.classList)
-
-if(menu.classList.contains('azul')){
-  console.log('possui a classe azul');
-} else {
-  console.log('não possui a classe azul');
-}
-
-const animais = document.querySelector('.animais');
-console.log(animais.attributes['id'] );
-
 const img = document.querySelector('img');
-console.log(img.getAttribute('src'));
+
+function callback(){
+  console.log('clicou');
+}
+img.addEventListener('click', callback);
+
+function callback2(e){
+  console.log(e);
+}
+img.addEventListener('click', callback2);
+
+const animaisLista = document.querySelector('.animais-lista');
+function callback3(e){
+  console.log(e.currentTarget);
+  console.log(e.target);
+}
+  animaisLista.addEventListener('click', callback3);
