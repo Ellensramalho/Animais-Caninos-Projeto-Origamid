479f0652758edfa9c5fe400f586c96d440d1aa72
Ellensramalho <ellen_santos_bsf@hotmail.com>
2026-01-20 15:14:31 -0300

    Função initTabNav

--- script.js
+++ script.js
@@ -1,40 +1,18 @@
-const h1 = document.querySelector('h1');
-//const animaisLista = document.querySelector('.animais-descricao');
+function initTabNav(){
+const tabMenu = document.querySelectorAll('.js-tabmenu li');
+const tabContent = document.querySelectorAll('.js-tabcontent section');
+tabContent[0].classList.add('ativo');
 
-h1.innerHTML;
-console.log(h1.innerHTML);
-//console.log(animaisLista.innerHTML);
-
-h1.outerHTML = '<p>Novo texto</p>';
-console.log(h1.outerHTML);
-
-const lista = document.querySelector('.animais-lista');
-
-console.log(lista.parentElement);
-console.log(lista.parentElement.parentElement);
-console.log(lista.nextElementSibling);
-console.log(lista.previousElementSibling);
-console.log(lista.children);
-console.log(lista.children[lista.children.length -1]);
-
-const contato = document.querySelector('.contato');
-const titulo = contato.querySelector('.titulo');
-const animais = document.querySelector('.animais');
-const mapa= document.querySelector('.mapa');
-
-contato.insertBefore(animais, titulo);
-
-const novoh1 = document.createElement('h1');
-console.log(novoh1);
-novoh1.innerHTML = 'Caninos são fofos';
-novoh1.classList.add('titulo');
-
-mapa.appendChild(novoh1);
-
-const h1 = document.querySelector('h1');
-const faq = document.querySelector('.faq');
-
-const cloneTitulo = h1.cloneNode(true);
-
-cloneTitulo.classList.add('azul');
-faq.appendChild(cloneTitulo);
+function ActiveTab(index){
+  tabContent.forEach((section) => {
+    section.classList.remove('ativo');
+  });
+  tabContent[index].classList.add('ativo');
+}
+tabMenu.forEach((itemMenu, index) => {
+  itemMenu.addEventListener('click',() => {
+    ActiveTab(index);
+  });
+});
+}
+initTabNav();
---
+++ seletores5.js
@@ -0,0 +1,40 @@
+const h1 = document.querySelector('h1');
+const animaisLista = document.querySelector('.animais-descricao');
+
+h1.innerHTML;
+console.log(h1.innerHTML);
+console.log(animaisLista.innerHTML);
+
+h1.outerHTML = '<p>Novo texto</p>';
+console.log(h1.outerHTML);
+
+const lista = document.querySelector('.animais-lista');
+
+console.log(lista.parentElement);
+console.log(lista.parentElement.parentElement);
+console.log(lista.nextElementSibling);
+console.log(lista.previousElementSibling);
+console.log(lista.children);
+console.log(lista.children[lista.children.length -1]);
+
+const contato = document.querySelector('.contato');
+const titulo = contato.querySelector('.titulo');
+const animais = document.querySelector('.animais');
+const mapa= document.querySelector('.mapa');
+
+contato.insertBefore(animais, titulo);
+
+const novoh1 = document.createElement('h1');
+console.log(novoh1);
+novoh1.innerHTML = 'Caninos são fofos';
+novoh1.classList.add('titulo');
+
+mapa.appendChild(novoh1);
+
+const h1 = document.querySelector('h1');
+const faq = document.querySelector('.faq');
+
+const cloneTitulo = h1.cloneNode(true);
+
+cloneTitulo.classList.add('azul');
+faq.appendChild(cloneTitulo);
