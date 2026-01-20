c69b94eff879f02c41dcec9d4f1dc67d6b23dc7f
Ellensramalho <ellen_santos_bsf@hotmail.com>
2026-01-20 15:35:40 -0300

    Função initAccordion

--- script.js
+++ script.js
@@ -16,3 +16,18 @@ tabMenu.forEach((itemMenu, index) => {
 });
 }
 initTabNav();
+function initAccordion(){
+const accordionList = document.querySelectorAll('.js-accordion dt');
+accordionList[0].classList.add('ativo');
+accordionList[0].nextElementSibling.classList.add('ativo');
+
+function activeAccordion(){
+  this.classList.toggle('ativo'); 
+  this.nextElementSibling.classList.toggle('ativo')
+}
+
+accordionList.forEach((item) => {
+  item.addEventListener('click', activeAccordion);
+});
+}
+initAccordion();
